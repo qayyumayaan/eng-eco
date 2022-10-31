056e0bf111983407c5fdd7803e462b3edfa46dd4
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2022-10-30 22:47:08 -0400

    Partial Full Implementation

--- RetireLib.java
+++ RetireLib.java
@@ -1,69 +1,69 @@
-public class RetirementCalculatorLib {
+public class RetireLib {
 
-    public static double FP(double P, int N, double i) {
+    public static double FP(double P, double i, int N) {
         // F/P
         double F = P * Math.pow(1 + i, N);
         return F;
     }
 
-    public static double PF(double F, int N, double i) {
+    public static double PF(double F, double i, int N) {
         // P/F
         double P = F / Math.pow(1 + i, N);
         return P;
     }
 
-    public static double FA(double A, int N, double i) {
+    public static double FA(double A, double i, int N) {
         // F/A
         double F = A / i * (Math.pow(1 + i, N) - 1);
         return F;
     }
 
-    public static double PA(double A, int N, double i) {
+    public static double PA(double A, double i, int N) {
         // P/A
         double intRate = Math.pow(1 + i, N);
         double P = A * (intRate - 1) / (i * intRate);
         return P;
     }
 
-    public static double AF(double F, int N, double i) {
+    public static double AF(double F, double i, int N) {
         // A/F
         double intRate = Math.pow(1 + i, N);
         double A = i / (intRate - 1);
         return A;
     }
 
-    public static double AP(double P, int N, double i) {
+    public static double AP(double P, double i, int N) {
         // A/P
         double intRate = Math.pow(1 + i, N);
         double A = i * intRate / (intRate - 1);
         return A;
     }
 
-    public static double PG(double G, int N, double i) {
+    public static double PG(double G, double i, int N) {
         // P/G
         double intRate = Math.pow(1 + i, N);
         double P = ((intRate - 1) / (i * intRate) - N / intRate) / i;
         return P;
     }
 
-    public static double AG(double G, int N, double i) {
+    public static double AG(double G, double i, int N) {
         // A/G
         double intRate = Math.pow(1 + i, N);
         double A = G * (1 / i - N / (intRate - 1));
         return A;
     }
 
-    public static double PAG(double G, int N, double i, double A) {
+    public static double PAG(double A, double G, double i, int N) {
         // Uniform Arithmetic Gradient
-        double P = PA(A, N, i) + PG(G, N, i);
+        double P = PA(A, i, N) + PG(G, i, N);
         return P;
     }
 
-    public static double PGGeo(double A, int N, double i, double f) {
+    public static double PGGeo(double A, double i, int N, double f) {
         // Geometric Gradient Series
         double P = 0;
         if (f == i) {
-            P = PF(A * N, N, i);
+            P = PF(A * N, i, N);
         } else if (f != i) {
             P = A * Math.abs(((1 - Math.pow((1 + i), -N) * Math.pow(1 + f, N)) / (i - f)));
         }
--- RetirementCalculator.java
+++ RetirementCalculator.java
@@ -1,3 +1,104 @@
 public class RetirementCalculator {
+    public static void main(String[] args) {
 
+        String[] order = {
+                "F/P",
+                "P/F",
+                "F/A",
+                "P/A",
+                "A/F",
+                "A/P",
+                "P/G",
+                "A/G",
+                "Present Arithmetic Gradient (P/A and P/G)",
+                "Present Geometric Gradient"
+        };
+
+        double P = 0;
+        double F = 0;
+        double i = 0;
+        double A = 0;
+        int N = 0;
+        double G = 0;
+        double f = 0;
+        double output = 0;
+        System.out.println(f % .2f);
+
+        boolean cont = true;
+        while (cont == true) {
+
+            StdOut.println("What would you like to calculate today?");
+
+            for (int j = 0; j < 10; j++) {
+                StdOut.println(j + ": " + order[j]);
+            }
+            StdOut.println("10: End the program. ");
+
+            int response = StdIn.readInt();
+            if (response == 10) {
+                StdOut.println("Thank you for using this program!");
+                cont = false;
+                break;
+            } else if (response == 0) { // F/P
+                StdOut.print("Please input P. ");
+                P = StdIn.readDouble();
+                StdOut.print("Please input i as a decimal. ");
+                i = StdIn.readDouble();
+                StdOut.print("Please input N. ");
+                N = StdIn.readInt();
+                output = RetireLib.FP(P, i, N);
+
+            } else if (response == 1) { // P/F
+                StdOut.print("Please input F. ");
+                F = StdIn.readDouble();
+                StdOut.print("Please input i as a decimal. ");
+                i = StdIn.readDouble();
+                StdOut.print("Please input N. ");
+                N = StdIn.readInt();
+                output = RetireLib.PF(F, i, N);
+
+            } else if (response == 2) { // F/A
+                StdOut.print("Please input A. ");
+                A = StdIn.readDouble();
+                StdOut.print("Please input i as a decimal. ");
+                i = StdIn.readDouble();
+                StdOut.print("Please input N. ");
+                N = StdIn.readInt();
+                output = RetireLib.FA(A, i, N);
+
+            } else if (response == 3) { // P/A
+                StdOut.print("Please input A. ");
+                A = StdIn.readDouble();
+                StdOut.print("Please input i as a decimal. ");
+                i = StdIn.readDouble();
+                StdOut.print("Please input N. ");
+                N = StdIn.readInt();
+                output = RetireLib.PA(A, i, N);
+
+            } else if (response == 4) { // A/F
+                StdOut.print("Please input F. ");
+                F = StdIn.readDouble();
+                StdOut.print("Please input i as a decimal. ");
+                i = StdIn.readDouble();
+                StdOut.print("Please input N. ");
+                N = StdIn.readInt();
+                output = RetireLib.AF(F, i, N);
+
+            } else if (response == 5) { // AP
+
+            } else if (response == 6) { // PG
+
+            } else if (response == 8) { // AG
+
+            } else if (response == 9) { // PAG
+
+            } else if (response == 10) { // PGGeo
+
+            } else {
+                StdOut.println("Invalid input. Please try again. ");
+            }
+
+        }
+
+    }
 }
